e88251857805d80e0a31c2952460b1c36d531fcc
Paulo H Perbone Rocha <paulohenriquerocha@yahoo.com>
2017-09-19 16:27:26 -0300

    Add comments to clarify the use of null object pattern

--- udao-core/src/main/java/io/perbone/udao/internal/NoCacheImpl.java
+++ udao-core/src/main/java/io/perbone/udao/internal/NoCacheImpl.java
@@ -36,144 +36,156 @@ final class NoCacheImpl implements Cache
     @Override
     public void add(final Object bean) throws IllegalArgumentException
     {
-
+        // no operation method
     }
 
     @Override
     public void add(final Object bean, final long ttl, final TimeUnit unit) throws IllegalArgumentException
     {
-
+        // no operation method
     }
 
     @Override
     public void set(final Object bean) throws IllegalArgumentException
     {
-
+        // no operation method
     }
 
     @Override
     public void set(final Object bean, long ttl, final TimeUnit unit) throws IllegalArgumentException
     {
-
+        // no operation method
     }
 
     @Override
     public boolean contains(final Object bean)
     {
+        // default no operation response
         return false;
     }
 
     @Override
     public boolean containsA(final String name, final Object... keys)
     {
+        // default no operation response
         return false;
     }
 
     @Override
     public boolean containsI(final Object id)
     {
+        // default no operation response
         return false;
     }
 
     @Override
     public boolean containsP(final Object... keys)
     {
+        // default no operation response
         return false;
     }
 
     @Override
     public long count()
     {
+        // default no operation response
         return 0;
     }
 
     @Override
     public void delete(final Object bean)
     {
-
+        // no operation method
     }
 
     @Override
     public void deleteA(final String name, final Object... keys)
     {
-
+        // no operation method
     }
 
     @Override
     public void deleteI(final Object id)
     {
-
+        // no operation method
     }
 
     @Override
     public void deleteP(final Object... keys)
     {
-
+        // no operation method
     }
 
     @Override
     public void evict()
     {
-
+        // no operation method
     }
 
     @Override
     public <T> T getA(final String name, final Object... keys)
     {
+        // default no operation response
         return null;
     }
 
     @Override
     public <T> List<T> getAll()
     {
+        // default no operation response
         return Collections.emptyList();
     }
 
     @Override
     public <T> T getI(final Object id)
     {
+        // default no operation response
         return null;
     }
 
     @Override
     public <T> T getP(final Object... keys)
     {
+        // default no operation response
         return null;
     }
 
     @Override
     public void invalidate()
     {
-
+        // no operation method
     }
 
     @Override
     public void invalidate(final Object criteria)
     {
-
+        // no operation method
     }
 
     @Override
     public void prune(final Object criteria)
     {
-
+        // no operation method
     }
 
     @Override
     public <T> T removeA(final String name, final Object... keys)
     {
+        // default no operation response
         return null;
     }
 
     @Override
     public <T> T removeI(final Object id)
     {
+        // default no operation response
         return null;
     }
 
     @Override
     public <T> T removeP(final Object... keys)
     {
+        // default no operation response
         return null;
     }
 }
