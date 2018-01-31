f28fb3008dad00ab2dea8223c657754be27c7bad
Paulo H Perbone Rocha <paulohenriquerocha@yahoo.com>
2018-01-31 15:50:12 -0200

    Update copyright year to 2018

--- udao-core/src/main/java/io/perbone/udao/internal/DataManagerFactoryImpl.java
+++ udao-core/src/main/java/io/perbone/udao/internal/DataManagerFactoryImpl.java
@@ -2,7 +2,7 @@
  * This file is part of UDAO 
  * https://github.com/perbone/udao/
  * 
- * Copyright 2013-2017 Paulo Perbone
+ * Copyright 2013-2018 Paulo Perbone
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
